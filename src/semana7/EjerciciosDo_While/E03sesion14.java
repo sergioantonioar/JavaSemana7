a3119024f7c2f626f28ed11c64f5beaf72e5afaf
Sergio Ariza <111243440+sergioantonioar@users.noreply.github.com>
2023-10-03 17:13:15 -0500

    Actualizando archivos

---
+++ src/semana7/EjerciciosDo_While/E03sesion14.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana7.EjerciciosDo_While;
+
+/**
+ * Escribir un programa que solicite un número “n” y muestre en pantalla los
+ * valores 1 y 0 dispuestos como se muestra en el ejemplo.
+ *
+ * @author sergio
+ */
+public class E03sesion14 {
+    
+}
