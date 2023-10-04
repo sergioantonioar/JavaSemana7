f3dbd51920e5f74863abfcf79b0d6fec6309b407
Sergio Ariza <111243440+sergioantonioar@users.noreply.github.com>
2023-10-03 19:25:03 -0500

    Agregando el ultimo ejercicio 3

--- src/semana7/EjerciciosDo_While/E03sesion14.java
+++ src/semana7/EjerciciosDo_While/E03sesion14.java
@@ -4,6 +4,8 @@
  */
 package semana7.EjerciciosDo_While;
 
+import java.util.Scanner;
+
 /**
  * Escribir un programa que solicite un número “n” y muestre en pantalla los
  * valores 1 y 0 dispuestos como se muestra en el ejemplo.
@@ -11,5 +13,30 @@ package semana7.EjerciciosDo_While;
  * @author sergio
  */
 public class E03sesion14 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        int n = sc.nextInt();
+
+        
+        int i = 1;
+        do {
+            int j = 1;
+
+            do {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+                j++;
+            } while (j <= i);
+
+            System.out.println();
+            i++;
+        } while (i <= n);
+        
+        
+    }
 }
